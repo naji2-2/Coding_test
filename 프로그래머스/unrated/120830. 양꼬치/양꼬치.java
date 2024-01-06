e8e8b0fa62d30f5c8f2b2e0943a3695c67596edd
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-01-07 00:01:52 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = (n*12000) + (k*2000);
+        if(n/10>=1){
+            answer = answer - ((n/10)*2000);
+        }
+        
+        return answer;
+    }
+}
