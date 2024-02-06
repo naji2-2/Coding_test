2b963cbcaf05c0b6ebb11ccc1a60315f53ea362c
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-02-06 23:45:18 +0900

    [level 0] Title: 문자열로 변환, Time: 0.02 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+       
+        answer = Integer.toString(n);
+        
+        return answer;
+    }
+}
