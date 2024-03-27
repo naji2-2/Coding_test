529892986c8bbd9d822e10ac3bebaee48f8047d2
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-03-27 23:12:00 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 1.66 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        
+        String answer = "";        
+        char[] arr = my_string.toCharArray();
+        
+        for(int i=0; i<arr.length; i++){
+            for(int j=1; j<=n; j++){
+                answer += String.valueOf(arr[i]);
+            }
+        }
+        
+        return answer;
+        
+    }
+}
