529e3eaa56f3abfccf0a085c996e6af928acd628
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-04-06 10:37:26 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = "";
-        return answer  = my_string.replace(letter, "");     // 특정문자를 ""로 교체
+        return my_string.replace(letter, "");     // 특정문자를 ""로 교체
     }
 }
