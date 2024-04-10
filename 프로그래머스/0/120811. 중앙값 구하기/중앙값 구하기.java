9145e5089e7e56c498e4f4978f6e84d272eec655
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-04-10 22:50:12 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.86 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,9 +1,7 @@
 import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;   
-        
-        Arrays.sort(array);                         // 배열정렬
-        return answer = array[array.length/2];      // 중앙 값 출력
+        Arrays.sort(array);                // 배열정렬
+        return array[array.length/2];      // 중앙 값 출력
     }
 }
