111a1856c1fefa7ffc70d160e4f9a8996fd392f6
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-04-12 01:12:23 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.44 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,7 +1,5 @@
 class Solution {
     public String[] solution(String my_string) {
-        my_string = my_string.trim();
-        my_string = my_string.replaceAll("\\s+"," ");
-        return my_string.split(" ");
+        return my_string.trim().split("\\s+");
     }
 }
