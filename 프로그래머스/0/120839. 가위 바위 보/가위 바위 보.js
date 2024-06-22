b4774704095b21fb6895a432dcb99f2b3ba48e5a
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-06-22 23:27:57 +0900

    [level 0] Title: 가위 바위 보, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,8 +1,7 @@
 function solution(rsp) {
     var answer = '';
-    var a = rsp.split('');
     
-    for(var x of a){
+    for(var x of rsp){
         if(x==='0') answer += '5';
         else if(x==='2') answer += '0';
         else answer += '2';
