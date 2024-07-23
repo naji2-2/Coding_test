e4371c17c2f4096acc49c58538a3649fd8873fc6
Na jisu <142985024+naji2-2@users.noreply.github.com>
2024-07-23 10:20:16 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 1.91 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -2,16 +2,21 @@ function solution(array) {
     var answer = [];
     
     var max = 0;
-    var index = 0;
+    let index = 0;
+    //var i = Number(0);
     
-    for(i in array){
+    for(var i in array){
         if(max<array[i]){
             max = array[i];
             index = i;
+            //index = parseInt(i);
+            //console.log(typeof index);
+            
         }        
     }
     answer.push(max);
-    answer.push(parseInt(index));
-
+    console.log(typeof index);
+    //-0을 적으면 숫자로 형변환 해줌
+    answer.push(index-0);
     return answer;
 }
