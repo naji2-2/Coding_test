a70bfd7d6ab724592e6e3569af0bdb8833645f1d
Na jisu <142985024+naji2-2@users.noreply.github.com>
2025-01-02 23:02:50 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.js
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,10 +1,8 @@
 function solution(names) {
     var answer = [];
     
-    for(var i=0; i<names.length; i+=5){
-        console.log(i);
+    for(var i=0; i<names.length; i+=5)
         answer.push(names[i]);
-    }
 
     return answer;
 }
