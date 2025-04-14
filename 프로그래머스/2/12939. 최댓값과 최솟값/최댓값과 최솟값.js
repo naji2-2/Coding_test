437f729c1d94713eb230ea91697b924b551c45e9
Na jisu <142985024+naji2-2@users.noreply.github.com>
2025-04-14 23:11:41 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,25 +1,9 @@
 function solution(s) {
     var answer = '';
-    let numbers = [];
     
-    let str = s.split(" ");
-    
-    for(let x of str){
-        numbers.push(parseInt(x));
-    }
-    
-    let max = parseInt(str[0]);
-    let min = parseInt(str[0]);
-    
-    for(let x of numbers){
-        if(x > max){
-            max = x;
-        } else if(x < min){
-            min = x;
-        }
-    }
-    
-    answer += min.toString().concat(" ", max.toString());
+    answer = s.split(" ").map(a => parseInt(a));
+
+    answer = Math.min(...answer) + " " + Math.max(...answer)
     
     return answer;
 }
